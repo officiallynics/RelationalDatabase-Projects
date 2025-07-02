663edf7b4681cc10f57e47eec2e781220521873f
Nicole Delgado <officiallynicshere@gmail.com>
2025-07-02 20:03:34 +0800

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -6,7 +6,7 @@ echo Enter your username:
 read USERNAME
 
 FIND_USERNAME=$($PSQL "SELECT username FROM players WHERE username='$USERNAME'")
-NUMBER_OF_GUESSES=0
+NUMBER_OF_GUESSES=1
 
 if [[ -z $FIND_USERNAME ]]
 then
@@ -18,7 +18,7 @@ then
     while [[ "$GUESS" != "$SECRET_NUMBER" ]]
     do
       ((NUMBER_OF_GUESSES++))
-      if [[ "$GUESS" =~ ^[+-]?[0-9]+$ ]]
+      if [[ ! "$GUESS" =~ ^[+-]?[0-9]+$ ]]
       then
         echo That is not an integer, guess again:
         read GUESS
@@ -36,6 +36,7 @@ then
     INSERT_TO_PLAYERS=$($PSQL "INSERT INTO players(username, games_played) VALUES('$USERNAME', 1)")
     PLAYER_ID=$($PSQL "SELECT id FROM players WHERE username='$USERNAME'")
     INSERT_TO_GAMES=$($PSQL "INSERT INTO games(user_id, no_of_guesses) VALUES($PLAYER_ID, $NUMBER_OF_GUESSES)")
+    
     #You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. Nice job!
     echo You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!
   
@@ -48,14 +49,14 @@ else
   echo Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
   SECRET_NUMBER=$(( RANDOM % 1001 ))
   echo -e "\nGuess the secret number between 1 and 1000:"
-    while [[ $GUESS -ne $SECRET_NUMBER ]]
+  read GUESS
+    while [[ "$GUESS" != "$SECRET_NUMBER" ]]
     do
       ((NUMBER_OF_GUESSES++))
-      
-      if [[ $GUESS =~ "^[+-]?[0-9]+$" ]]
+      if [[ ! "$GUESS" =~ ^[+-]?[0-9]+$ ]]
       then
         echo That is not an integer, guess again:
-        
+        read GUESS
       else
         if [[ $GUESS -gt $SECRET_NUMBER ]]
         then
@@ -63,11 +64,10 @@ else
         else 
           echo "It's higher than that, guess again:"
         fi 
+         read GUESS
       fi
-      read GUESS
     done
-      #INSERT_TO_GAMES=$($PSQL "INSERT INTO games(user_id, no_of_guesses) VALUES($USER_ID, $NUMBER_OF_GUESSES)")
-      #UPDATE_GAMES=$($PSQL "UPDATE players SET games_played=$GAMES_PLAYED+1 WHERE username='$USERNAME'")
+    INSERT_TO_GAMES=$($PSQL "INSERT INTO games(user_id, no_of_guesses) VALUES($USER_ID, $NUMBER_OF_GUESSES)")
+    UPDATE_GAMES=$($PSQL "UPDATE players SET games_played=$GAMES_PLAYED+1 WHERE username='$USERNAME'")
     echo You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!
 fi 
-
